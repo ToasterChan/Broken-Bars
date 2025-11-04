eff595462c551a2164f11c63bc5fa36bee642ba2
Afonso Teca <P2923845@LEC-ADMIN.dmu.ac.uk>
2025-11-04 16:34:50 +0000

    added a UI script

---
+++ broken bars/Assets/Scripts/HudManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HudManager : MonoBehaviour
+{
+
+    public Text Maintext;
+    public Button Option1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Maintext.text = "welcome o my game";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
