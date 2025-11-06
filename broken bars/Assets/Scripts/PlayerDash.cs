bd309557200388c7fa3a74281ac0483504e21f2d
Dom Simmonds <P2924625@LEC-ADMIN.dmu.ac.uk>
2025-11-06 13:04:13 +0000

    dash bugged

--- broken bars/Assets/Scripts/PlayerDash.cs
+++ broken bars/Assets/Scripts/PlayerDash.cs
@@ -4,15 +4,62 @@ using UnityEngine;
 
 public class Dash : MonoBehaviour
 {
+    public float dashSpeed = 20f; // Speed of the dash
+    public float dashDuration = 0.2f;  // How long the dash lasts
+    public float dashCooldown = 1f; //Time befrore the player can dash again
+
+    private Rigidbody2D rb;
+    private bool isDashing = false;
+    private float dashTime;
+    private float lastDashTime;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Check for dash input and cooldown
+        if (Input .GetKeyDown(KeyCode.Q) && Time.time >= lastDashTime + dashCooldown)
+        {
+            StartDash();
+        }
     }
-}
+
+    void FixedUpdate()
+    {
+        if (isDashing)
+        {
+          Dash();
+
+        }
+    }
+
+    void StartDash()
+    {
+        isDashing = true;
+        dashTime = Time.time + dashDuration;
+        lastDashTime = Time.time;
+    }
+
+    void Dash()
+    {
+        // Apply dash movement in the direction of input
+        if (Time.time < dashTime)
+        {
+            Vector2 dashDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+            rb.velocity = dashDirection * dashSpeed;
+
+            // End dash after duration
+            if (Time.time >= dashTime)
+            {
+                isDashing = false;
+                rb.velocity = Vector2.zero; // Stop movement after dash
+
+
+
+
+    
